bf8dfa6c6c5b28605bfdda51ea462cc2439af459
bkampdev <bkampproducer@gmail.com>
2024-11-06 08:48:39 -0300

    Add initial project files including HTML structure, CSS styles, and media assets

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Em manutenção</title>
+    <link rel="shortcut icon" type="imagex/png" href="./images/icon.ico">
+    <link rel="stylesheet" href="css/style.css">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');</style>
+    <link rel="stylesheet" href="fonts/stylesheet.css" type="text/css" charset="utf-8" />
+</head>
+<body>
+    <div class="video-container">
+        <video autoplay loop muted>
+            <source src="src/videos/Bkamp_world.mp4">
+        </video>
+    </div>
+    <div class="titulo">
+        <span class="blackish">Bkamp Wrld</span>
+        <p><i>Em <b>manutenção</b></i></p>
+    </div>
+</body>
+</html>
